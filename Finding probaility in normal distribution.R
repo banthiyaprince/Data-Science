15750cf3937ef3e4b20ecaeeda9c3eb748b2251b
banthiyaprince <65126599+banthiyaprince@users.noreply.github.com>
2020-05-12 14:02:25 +0530

    Add files via upload

---
+++ Finding probaility in normal distribution.R
@@ -0,0 +1,7 @@
+#probability in normal distribution
+#continuos random variable probability is alays 0.
+# It is always in relative manner
+#pnorm(Random variable,Sample mean,std deviation)
+pnorm(740,711,29)
+pnorm(697,711,29)
+
